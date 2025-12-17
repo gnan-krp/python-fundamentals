6c162c49825d2e98ee91c2ec0f01b30905a15b2a
gnan-krp <gnan0839@gmail.com>
2025-12-17 18:34:39 +0530

    Update variable.py

--- basics/variable.py
+++ basics/variable.py
@@ -1,3 +1,4 @@
+*\First Basic Code That I learned In python fundamentals/*
 name = "Gnan"
 age = 18
 height = 5'8
@@ -5,3 +6,10 @@ height = 5'8
 print("Your Name  =",name)
 print("Your Age  =",age)
 print("Your Height  =",height)
+
+*\Second Basic Code That I learned In python fundamentals/*
+var = 1
+account_balance = 1000.0
+client_name = 'John Doe'
+print(var, account_balance, client_name)
+print(var)
